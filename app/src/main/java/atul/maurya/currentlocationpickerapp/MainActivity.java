4f279a1b9fd393e4fe023919a2f4a1b5b6973f18
atulproject99 <71038525+atulproject99@users.noreply.github.com>
2021-08-26 01:36:14 +0530

    first commit

---
+++ app/src/main/java/atul/maurya/currentlocationpickerapp/MainActivity.java
@@ -0,0 +1,66 @@
+package atul.maurya.currentlocationpickerapp;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.content.res.Resources;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+import atul.maurya.currentlocationpickerapp.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final int ADDRESS_PICKER_REQUEST = 1054;
+    ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+    }
+
+    public void getCurrentLocation(View view) {
+        Intent intent = new Intent(MainActivity.this, MyLocationPickerActivity.class);
+        startActivityForResult(intent, ADDRESS_PICKER_REQUEST);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == ADDRESS_PICKER_REQUEST) {
+            try {
+                if (data != null && data.getStringExtra(MapUtility.ADDRESS) != null) {
+                    // String address = data.getStringExtra(MapUtility.ADDRESS);
+                    double currentLatitude = data.getDoubleExtra(MapUtility.LATITUDE, 0.0);
+                    double currentLongitude = data.getDoubleExtra(MapUtility.LONGITUDE, 0.0);
+                    Bundle completeAddress = data.getBundleExtra("fullAddress");
+                    /* data in completeAddress bundle
+                    "fulladdress"
+                    "city"
+                    "state"
+                    "postalcode"
+                    "country"
+                    "addressline1"
+                    "addressline2"
+                     */
+
+                    binding.location.setText(new StringBuilder().append("addressline2: ").append
+                            (completeAddress.getString("addressline2")).append("\ncity: ").append
+                            (completeAddress.getString("city")).append("\npostalcode: ").append
+                            (completeAddress.getString("postalcode")).append("\nstate: ").append
+                            (completeAddress.getString("state")).toString());
+
+                    Log.e("sdd", new StringBuilder().append("Lat:").append(currentLatitude).append
+                            ("  Long:").append(currentLongitude).toString());
+
+                }
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
+        }
+    }
+}
